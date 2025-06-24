b18b15df4fe19fa7619a04ccba7bc395c27e2ad0
Mahantesh Doddamani <mahanteshdoddamani@Mahanteshs-MacBook-Air.local>
2025-06-24 14:15:29 +0530

    [Update]: resolved indentation errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,7 +2,8 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add x and y and return results"""
     return x + y
 
--- app/app/tests.py
+++ app/app/tests.py
@@ -17,4 +17,3 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, -5)
-        
